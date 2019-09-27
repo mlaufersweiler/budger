6d113ef989875013596714f2590782ff7d434f80
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-09-26 23:19:12 -0400

    add home and footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import Footer from "../Footer";
+import "./Home.css";
 
-class Home extends Component {
+export class Home extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="title">
+        <h2 className="title-2">Festville</h2>
+        <img
+          className="picture"
+          src="https://images.pexels.com/photos/7640/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="breath therapy"
+          height="65%"
+          width="100%"
+          class="responsive-img"
+        />
+
+        <div>
+          <Footer />
+        </div>
+      </div>
+    );
   }
 }
 
